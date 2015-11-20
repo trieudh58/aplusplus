dd36f1894241e72266cf221975fe12d141143894
hungdn58 <hoangnhat1090@gmail.com>
2015-11-20 22:31:23 +0700

    add splashscreenactivity to revproject

---
+++ app/src/main/java/com/example/hoang/revproject/Activity/SplashScreenActivity.java
@@ -0,0 +1,40 @@
+package com.example.hoang.revproject.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.hoang.revproject.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().hide();
+
+        ImageView logo = (ImageView) findViewById(R.id.imgLogo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+
+}
