b74adeba545c8a87fb56bb40bd84e47c8b64b712
ducvn58 <vungocduc24101995@gmail.com>
2015-11-27 19:48:41 +0700

    edit database from game

--- app/src/main/java/com/example/hoang/revproject/Activity/TroChoi.java
+++ app/src/main/java/com/example/hoang/revproject/Activity/TroChoi.java
@@ -24,6 +24,7 @@ public class TroChoi extends AppCompatActivity {
     LinearLayout linear1, linear2, linear3, linear4;
     List <VocabularyModel> listVocab;
     AlarmDBHelper alarmDBHelper;
+    ImageView image ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +34,58 @@ public class TroChoi extends AppCompatActivity {
         linear2 = (LinearLayout)findViewById(R.id.linear2);
         linear3 = (LinearLayout)findViewById(R.id.linear3);
         linear4 = (LinearLayout)findViewById(R.id.linear4);
+        image = (ImageView) findViewById(R.id.image);
 
-        khoiTaoManChoi(linear2, linear3, linear4, "songxobo");
         alarmDBHelper = new AlarmDBHelper(this);
         listVocab = alarmDBHelper.getListVocabs();
+        Random rd = new Random();
+        int index = rd.nextInt(listVocab.size());
+
+        VocabularyModel model = listVocab.get(index);
+
+        String tuSauChuanHoa = chuanHoa(model.getWord());
+
+        for (int i = 0; tuSauChuanHoa.length() > 9; i++) {
+
+            index = rd.nextInt(listVocab.size());
+
+            model = listVocab.get(index);
+
+            tuSauChuanHoa = chuanHoa(model.getWord());
+        }
+
+        khoiTaoManChoi(linear2, linear3, linear4, tuSauChuanHoa);
+
+        int imageResource = this.getResources().getIdentifier(model.getImagePath(), null, this.getPackageName());
+        Drawable res = this.getResources().getDrawable(imageResource);
+        image.setImageDrawable(res);
+
+
+    }
+
+    public String chuanHoa(String tu) {
+
+        String tuSauChuanHoa;
+        tuSauChuanHoa = tu.toLowerCase().trim();
+        char temp[] = tuSauChuanHoa.toCharArray();
+        for (int i = 0; i < temp.length; i++) {
+
+            if (temp[i] == ' ') {
+
+                for (int j = i; j < temp.length - 1; j++) {
+
+                    temp[j] = temp[j + 1];
+                }
+
+                temp[temp.length - 1] = '\0';
+                i--;
+            }
+        }
+
+        tuSauChuanHoa = tuSauChuanHoa.valueOf(temp);
+        tuSauChuanHoa = tuSauChuanHoa.trim();
+
+        return tuSauChuanHoa;
     }
 
     public void khoiTaoManChoi(LinearLayout linear1, LinearLayout linear2, LinearLayout linear3, String tumoi) {
@@ -52,21 +101,32 @@ public class TroChoi extends AppCompatActivity {
 
     public Button[] khoiTaoOChu(LinearLayout linear, String a) {
 
-        Button[] btnOChu = new Button[a.length()];
+        int temp;
+        if (a.length()%2 == 0)
+            temp = 8;
+        else
+            temp = 9;
 
-        for (int i = 0; i < a.length(); i++) {
+        Button[] btnOChu = new Button[temp];
+
+        for (int i = 0; i < temp; i++) {
 
             btnOChu[i] = new Button(this);
 
             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.MATCH_PARENT);
-            lp.setMargins(10, 0, 0, 0);
+            lp.setMargins(5, 0, 5, 0);
             lp.weight = 1;
             btnOChu[i].setLayoutParams(lp);
             btnOChu[i].setBackgroundResource(R.drawable.btn_toper);
             btnOChu[i].setTextColor(Color.BLACK);
             btnOChu[i].setTextSize(18);
             btnOChu[i].setGravity(Gravity.CENTER);
+
+            if (i < (temp - a.length()) / 2 || i >= (temp + a.length()) / 2) {
+                btnOChu[i].setVisibility(View.INVISIBLE);
+                btnOChu[i].setEnabled(false);
+            }
             linear.addView(btnOChu[i]);
         }
 
@@ -84,7 +144,7 @@ public class TroChoi extends AppCompatActivity {
             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             lp.weight = 1;
-            lp.setMargins(14,5,5,5);
+            lp.setMargins(6, 5, 6, 0);
             btn[i].setLayoutParams(lp);
             btn[i].setTextColor(Color.WHITE);
             btn[i].setTextSize(18);
@@ -143,7 +203,7 @@ public class TroChoi extends AppCompatActivity {
 
                     for (int j = 0; j < n; j++) {
 
-                        if (btnOChu[j].getText() == "") {
+                        if (btnOChu[j].getText() == "" && j >= (btnOChu.length - tu.length()) / 2 && j < (btnOChu.length + tu.length()) / 2) {
 
                             btnOChu[j].setText(btn[a].getText());
 
@@ -151,11 +211,11 @@ public class TroChoi extends AppCompatActivity {
 
                             t[j] = a;
 
-                            if ( !chuoiTv(btnOChu).equals(tu) && !chuoiTv(btnOChu).equals("") ) {
+                            if ( !chuoiTv(btnOChu, tu.length()).equals(tu) && !chuoiTv(btnOChu, tu.length()).equals("") ) {
 
                                 Toast toast = Toast.makeText(TroChoi.this, "Từ mới chưa đúng!", Toast.LENGTH_SHORT);
                                 toast.show();
-                            } else if ( chuoiTv(btnOChu).equals(tu) ) {
+                            } else if ( chuoiTv(btnOChu, tu.length()).equals(tu) ) {
 
                                 AlertDialog.Builder dialog = new AlertDialog.Builder(TroChoi.this);
                                 dialog.setTitle("Chính xác!!!");
@@ -164,20 +224,9 @@ public class TroChoi extends AppCompatActivity {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
 
-                                        for (int i = 0; i < n; i++) {
-
-                                            linear.removeView(btnOChu[i]);
-                                        }
-
-                                        for (int i = 0; i < 14; i++) {
-
-                                            if (i < 7)
-                                                linear1.removeView(btn[i]);
-                                            else
-                                                linear2.removeView(btn[i]);
-                                        }
-
-                                        khoiTaoManChoi(linear, linear1, linear2, "songdaica");
+                                        Intent intent = getIntent();
+                                        finish();
+                                        startActivity(intent);
                                     }
                                 });
 
@@ -214,13 +263,19 @@ public class TroChoi extends AppCompatActivity {
         }
     }
 
-    public String chuoiTv(TextView tv[]) {
+    public String chuoiTv(TextView tv[], int doDaiTu) {
 
         String a;
         a = "";
 
+        int temp;
+        if (doDaiTu%2 == 0)
+            temp = 8;
+        else
+            temp = 9;
+
         for (int i = 0; i < tv.length; i++) {
-            if (tv[i].getText() == "")
+            if (i >= (temp - doDaiTu) / 2 && i < (temp + doDaiTu) / 2 && tv[i].getText() == "")
                 return "";
             else
                 a += tv[i].getText();
